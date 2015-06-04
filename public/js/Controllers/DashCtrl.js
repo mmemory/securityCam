13fe2699a1871ade498c88126582978f9892e278
Dustin Myers <dustint314@gmail.com>
2015-06-04 11:04:38 -0600

    Skeleton Templates

---
+++ public/js/Controllers/DashCtrl.js
@@ -0,0 +1,51 @@
+var app = angular.module('securityCam')
+	.controller('DashCtrl', ['$scope', '$timeout', '$mdSidenav', '$mdUtil', '$log', function($scope, $timeout, $mdSidenav, $mdUtil, $log) {
+
+	
+	////////////////////////////
+	// 		  On the Scope		
+	///////////////////////////
+	$scope.submissionStartDate
+	$scope.submissionEndDate
+	$scope.group
+
+
+	////////////////////////////
+	// 		  Sidebar		
+	///////////////////////////
+
+    $scope.toggleLeft = buildToggler('left');
+    $scope.toggleRight = buildToggler('right');
+
+    /**
+     * Build handler to open/close a SideNav; when animation finishes
+     * report completion in console
+     */
+    function buildToggler(navID) {
+      var debounceFn =  $mdUtil.debounce(function(){
+            $mdSidenav(navID)
+              .toggle()
+              .then(function () {
+                $log.debug("toggle " + navID + " is done");
+              });
+          },300);
+      return debounceFn;
+    }
+  
+    $scope.closeLeft = function () {
+      $mdSidenav('left').close()
+        .then(function () {
+          $log.debug("close LEFT is done");
+        });
+    };
+
+    $scope.groups = [
+          "Group A",
+          "Group B",
+          "Group C",
+          "Group D"
+      ];
+
+    /////////// End Sidebar ////////////
+
+}]) // End MainCtrl //
---
+++ public/js/mainApp.js
@@ -0,0 +1,27 @@
+var app = angular.module('securityCam', ['ngMaterial', 'ui.router'])
+
+	.config(['$mdThemingProvider', '$stateProvider', '$urlRouterProvider', function($mdThemingProvider, $stateProvider, $urlRouterProvider) {
+  	$mdThemingProvider.theme('default')
+    	.primaryPalette('light-blue')
+    	.accentPalette('blue')
+
+  	$urlRouterProvider.otherwise('/');
+
+  	$stateProvider
+  		.state('dashboard', {
+  			url: '/dashboard',
+  			templateUrl: 'js/Templates/DashTmpl.html',
+  			controller: 'DashCtrl'
+  		})
+  		.state('admin', {
+  			url: '/admin',
+  			templateUrl: 'js/Templates/AdminTmpl.html',
+  			controller: 'AdminCtrl'
+  		})
+  		.state('welcome', {
+  			url: '/welcome',
+  			templateUrl: 'js/Templates/WelcomeTmpl.html',
+  			controller: 'WelcomeCtrl'
+  		})
+
+  }]) // end config //
