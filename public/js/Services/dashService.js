9681302455e291ddf1b4edbecdeec61a58f4b64c
Dustin Myers <dustint314@gmail.com>
2015-06-04 12:34:51 -0600

    changed to CDNs

--- public/js/Services/dashService.js
+++ public/js/Services/dashService.js
@@ -1,6 +1,5 @@
-var app = angular.module('securityCam');
-
-app.service('dashService', function($http, $q) {
+var app = angular.module('securityCam')
+.service('dashService', function($http, $q) {
 
 	this.cameraInfo = function(startDate, endDate) {
 		var defer = $q.defer();
