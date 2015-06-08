62b7cd0f7659966f22e5402e917787ac1b3ed709
guybrud <guybrud@gmail.com>
2015-06-08 11:27:20 -0600

    split registration and login, made small mockup, working on bugs

--- public/js/Controllers/LoginCtrl.js
+++ public/js/Controllers/LoginCtrl.js
@@ -1,15 +1,15 @@
 var app = angular.module('securityCam')
 	.controller('LoginCtrl', ['$scope', 'LoginService', function($scope, LoginService) {
 	
-	$scope.clickRegister = function() {
-        console.log('controller register called');
-		LoginService.signup($scope.firstname, $scope.lastname, $scope.email, $scope.password).then(function() {
-			$location.path('/admin');
+	$scope.clickLogin = function() {
+		console.log('controller login called');
+		LoginService.login($scope.email, $scope.password).then(function() {
+			$location.path('/dashboard');
 		}).catch(function(err) {
-			$scope.error = err;
 			console.log($scope.error);
 		})
-			
+
 	}
+	
 
 }]) // End RegisCtrl.js //
