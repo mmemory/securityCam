3100e197472f8381ff27b610dad21cf7f1814678
guybrud <guybrud@gmail.com>
2015-06-08 17:44:34 -0600

    login page working correctly, need to set it up so you cant make it to the dashboard unless you are logged in

--- public/js/Controllers/RegisterCtrl.js
+++ public/js/Controllers/RegisterCtrl.js
@@ -1,13 +1,16 @@
 var app = angular.module('securityCam')
-	.controller('RegisterCtrl', ['$scope', 'LoginService', function($scope, LoginService) {
+	.controller('RegisterCtrl', ['$scope', 'LoginService', '$location', function($scope, LoginService, $location) {
 	
 	$scope.clickRegister = function() {
         console.log('controller register called');
 		LoginService.signup($scope.firstname, $scope.lastname, $scope.email, $scope.password).then(function() {
-			$location.path('/admin');
+			
+			$location.path('admin');
+
 		}).catch(function(err) {
 			$scope.error = err;
 			console.log($scope.error);
+			document.getElementsByClassName('erase').value = ""
 		})
 			
 	}
