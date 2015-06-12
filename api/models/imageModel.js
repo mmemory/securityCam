281db8cab44b308cd072ae227e5ed94c1259a804
Michael Memory <michaelmemoria@gmail.com>
2015-06-12 11:46:09 -0600

    More Testing

--- api/models/imageModel.js
+++ api/models/imageModel.js
@@ -2,8 +2,10 @@ var mongoose = require('mongoose');
 
 var ImageModel = new mongoose.Schema({
     name: {type: String, required: true},
-    created_on: {type: Date},
-    from_hardware: {type: mongoose.Schema.Types.ObjectId, ref: 'Hardware'}
+    //created_on: {type: Date},
+    //from_hardware: {type: mongoose.Schema.Types.ObjectId, ref: 'Hardware'}
+    from_hardware: String,
+    image_url: String
 });
 
 module.exports = mongoose.model('Image', ImageModel);
