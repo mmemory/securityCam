fa9d85035302231ae3f5663972e923241013f104
Dustin Myers <dustint314@gmail.com>
2015-06-16 11:27:51 -0600

    landing page work

--- public/js/Controllers/WelcomeCtrl.js
+++ public/js/Controllers/WelcomeCtrl.js
@@ -1,7 +1,70 @@
 var app = angular.module('securityCam')
-	.controller('WelcomeCtrl', ['$scope', 'user', function($scope, user) {
+	.controller('WelcomeCtrl', ['$scope', '$mdDialog', '$location', '$document', function($scope, $mdDialog, $location, $document) {
 	
-		$scope.user = user;
+	$scope.login = function(ev) {
+		console.log("Login clicked WelcomeCtrl")
+    $mdDialog.show({
+      controller: RegisterController,
+      parent: angular.element(document.body),
+      clickOutsideToClose: true,
+      title: 'Login',
+      templateUrl: 'js/Templates/loginDialog.html',
+      targetEvent: ev
+  	})
+    .then(function() {
+    	console.log('logging in...')
+    })
+	};
+
+	$scope.register = function(ev) {
+		console.log("Register clicked WelcomeCtrl")
+    $mdDialog.show({
+      controller: RegisterController,
+      parent: angular.element(document.body),
+      clickOutsideToClose: true,
+      title: 'Register',
+      templateUrl: 'js/Templates/registerDialog.html',
+      targetEvent: ev
+  	})
+    .then(function() {
+    	console.log('registering...')
+    })
+  };
+
+  function RegisterController($scope, $mdDialog) {
+	  $scope.register = function(firstname, lastname, email, groupName, password) {
+	    $mdDialog.hide();
+	    console.log('addUser invoked', firstname, lastname, email, groupName, password);
+	    LoginService.signup(firstname, lastname, email, groupName, password).then(function() {
+	    	$location.path('dashboard');
+	    })
+	    .catch(function(err) {
+	    	$scope.error = err;
+	    	console.log($scope.error);
+	    })
+	  };
+
+	  $scope.login = function(email, password) {
+	    $mdDialog.hide();
+	    console.log('addUser invoked', email, password);
+	    LoginService.login($scope.email, $scope.password).then(function() {
+				$location.path('dashboard');
+			}).catch(function(err) {
+				console.log($scope.error);
+				$scope.error="There was an error with the Username or Password, please try again!"
+			})
+	  };
+
+	  $scope.closeDialog = function() {
+	    $mdDialog.hide();
+	    console.log('registration cancelled')
+	  };
+	};
+
+	$scope.scroll = function() {
+		console.log('scroll clicked')
+		$document.scrollToElementAnimated('section');
+	};
 	
 
 }]) // End MainCtrl //
