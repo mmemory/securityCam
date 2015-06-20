12ec28d0a1b22a365ae87cb6f1f0f1168067ab1e
Michael Memory <michaelmemoria@gmail.com>
2015-06-19 22:15:05 -0600

    Correction for testing

--- api/models/imageModel.js
+++ api/models/imageModel.js
@@ -6,7 +6,7 @@ var ImageModel = new mongoose.Schema({
     //from_hardware: {type: mongoose.Schema.Types.ObjectId, ref: 'Hardware'}
     from_hardware: String,
     image_url: String,
-    lightIntensity: {type: String},
+    //lightIntensity: {type: String},
     temperatureInCelsius: {type: String}
     //temperatureInFahrenheit: {type: String},
     //humidity: {type: String}
