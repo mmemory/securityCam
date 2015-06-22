a7f23e984395f787c6652362b7ac01afc3a165a8
guybrud <guybrud@gmail.com>
2015-06-22 16:48:50 -0600

    fixed register, add users

--- public/js/Services/AdminService.js
+++ public/js/Services/AdminService.js
@@ -1,6 +1,5 @@
 var app = angular.module('securityCam')
-    .service('AdminService', ['$http', '$q',
-        function($http, $q) {
+    .service('AdminService', ['$http', '$q', function($http, $q) {
 
             this.registerUser = function(firstname, lastname, email, password) {
                 console.log('hit AdminService called');
@@ -23,7 +22,27 @@ var app = angular.module('securityCam')
                 });
                 return deferred.promise;
             };
-
+            this.registerMember = function(firstname, lastname, email, password) {
+                console.log('hit AdminService called');
+                var deferred = $q.defer();
+                $http({
+                    method: 'POST',
+                    url: '/api/user/member',
+                    data: {
+                        firstName: firstname,
+                        lastName: lastname,
+                        email: email,
+                        password: password
+                    }
+                }).then(function(res) {
+                    deferred.resolve(res.data);
+                    console.log(res.data);
+                }).catch(function(res) {
+                    deferred.reject(res.data);
+                    console.log(res.data);
+                });
+                return deferred.promise;
+            };
             this.deleteMember = function(email) {
                 var deferred = $q.defer();
                 $http({
