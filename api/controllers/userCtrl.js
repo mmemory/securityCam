f0f1d534dcddc43ea955c8ceec0bdfb96289eec8
Michael Memory <michaelmemoria@gmail.com>
2015-06-22 12:02:40 -0600

    Added populate to user control

--- api/controllers/userCtrl.js
+++ api/controllers/userCtrl.js
@@ -10,6 +10,8 @@ module.exports = {
             .populate('group_admin')
             .populate('group_member')
             .populate('group_admin.hardware_registered')
+            .populate('group_admin.pictures_total')
+            .populate('group_member.pictures_total')
             .exec(function(err, userFromMongo) {
                 res.send(userFromMongo);
             });
