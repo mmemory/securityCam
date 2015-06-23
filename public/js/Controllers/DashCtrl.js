f280170587d23be97ddd6d14314ec2e08e5e8f80
guybrud <guybrud@gmail.com>
2015-06-22 20:42:18 -0600

    errors fixed

--- public/js/Controllers/DashCtrl.js
+++ public/js/Controllers/DashCtrl.js
@@ -67,13 +67,15 @@ var app = angular.module('securityCam')
             console.log($scope.user);
 
             var displayGroups = function(groups) {
-                groupAdminName = $scope.user.group_admin.name;
-                groupName = $scope.user.group_member;
-                groups.unshift(groupAdminName);
-                for (var i = 0; i < groupName.length; i++) {
-                    groups.push(groupName[i])
+                var groupName = $scope.user.group_member;
+                var groupAdminName = $scope.user.group_admin.name;
+                if (groupAdminName) {
+                    groups.unshift(groupAdminName);
                 };
-                console.log($scope.user.group_member)
+                for (var i = 0; i < groupName.length; i++) {
+                    groups.push(groupName[i]);
+                };
+                console.log($scope.user.group_member);
             };
             displayGroups($scope.groups);
 
